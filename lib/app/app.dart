b4980744b7bfee95e0fa6183a684dd4072d9f95c
kungpotato <kittisak_p@scasset.com>
2023-11-10 11:03:52 +0700

    init

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,16 +2,12 @@ import 'dart:async';
 
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:emer_app/app/app_home.dart';
-import 'package:emer_app/app/authentication/login_page.dart';
 import 'package:emer_app/app/authentication/splash_page.dart';
 import 'package:emer_app/app/authentication/store/auth_store.dart';
 import 'package:emer_app/app/locale/locale_store.dart';
 import 'package:emer_app/app/storage/theme_storeage.dart';
 import 'package:emer_app/app/theme/app_theme.dart';
 import 'package:emer_app/l10n/l10n.dart';
-import 'package:emer_app/pages/pin_verify_page.dart';
-import 'package:emer_app/pages/user_profile_form_page.dart';
-import 'package:emer_app/pages/verify_email_page.dart';
 import 'package:emer_app/shared/extensions/context_extension.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +16,6 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx/mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:reactive_forms/reactive_forms.dart';
-import 'package:rxdart/rxdart.dart';
 
 class App extends StatefulWidget {
   const App({this.adaptiveThemeMode, super.key});
@@ -44,39 +39,13 @@ class AppState extends State<App> {
     super.initState();
     ThemePreferences.instance.init();
     WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      subAuth = _auth.authStateChanges().flatMap((user) {
+      subAuth = _auth.authStateChanges().listen((user) {
         if (user != null) {
           context.authStore.setUSer(user);
-          return context.authStore.getDbProfile(user).flatMap((dbUser) {
-            if (dbUser != null) {
-              context.authStore.setProfile(dbUser);
-              return context.authStore
-                  .getHealthInfo(dbUser.id!)
-                  .flatMap((info) {
-                // if (dbUser.verify != true &&
-                //     dbUser.role == ProfileRole.doctor) {
-                //   context.authStore.setVerifyState();
-                // } else
-                if (info != null) {
-                  if (user.emailVerified) {
-                    context.authStore.setAuthenticateState();
-                  } else {
-                    context.authStore.setVerifyEmailState();
-                  }
-                }
-                return Stream.value(dbUser);
-              });
-            }
-            context.authStore.setProfile(null);
-            context.authStore.setUserInfoState();
-            return Stream.value(null);
-          });
+          context.authStore.setSignIn();
         } else {
-          context.authStore.setSignOutState();
-          return Stream.value(null);
+          context.authStore.login('admin@mail.com', '12345678');
         }
-      }).listen((user) {
-        //
       });
 
       watchAuthState();
@@ -86,38 +55,12 @@ class AppState extends State<App> {
   void watchAuthState() {
     final authStore = Provider.of<AuthStore>(context, listen: false);
     disposer = reaction((_) => authStore.status, (status) {
-      if (status == AuthStatus.unauthenticated) {
-        _navigator.pushAndRemoveUntil(
-          MaterialPageRoute<dynamic>(
-            builder: (context) => const LoginPage(),
-          ),
-              (route) => false,
-        );
-      } else if (status == AuthStatus.userInfo) {
-        _navigator.pushReplacement(
-          MaterialPageRoute<void>(
-            builder: (context) => const UserProfileFormPage(),
-          ),
-        );
-      } else if (status == AuthStatus.verify) {
-        _navigator.push(
-          MaterialPageRoute<void>(
-            builder: (context) => const PinVerifyPage(),
-          ),
-        );
-      } else if (status == AuthStatus.verifyEmail) {
-        _navigator.pushAndRemoveUntil(
-          MaterialPageRoute<dynamic>(
-            builder: (context) => VerifyEmailPage(),
-          ),
-              (route) => false,
-        );
-      } else if (status == AuthStatus.authenticated) {
+      if (status == AuthStatus.authenticated) {
         _navigator.pushAndRemoveUntil(
           MaterialPageRoute<dynamic>(
             builder: (context) => const AppHome(),
           ),
-              (route) => false,
+          (route) => false,
         );
       }
     });
@@ -138,37 +81,33 @@ class AppState extends State<App> {
       light: MyThemes.lightTheme,
       dark: MyThemes.darkTheme,
       initial: widget.adaptiveThemeMode ?? AdaptiveThemeMode.light,
-      builder: (light, dark) =>
-          Observer(
-            builder: (context) =>
-                ReactiveFormConfig(
-                  validationMessages: {
-                    ValidationMessage.required: (
-                        error) => 'Should not be empty',
-                    ValidationMessage.email: (error) => 'Invalid email',
-                  },
-                  child: MaterialApp(
-                    title: 'My App',
-                    theme: light,
-                    darkTheme: dark,
-                    localizationsDelegates: [
-                      AppLocalizations.delegate,
-                      GlobalMaterialLocalizations.delegate,
-                      GlobalWidgetsLocalizations.delegate,
-                      GlobalCupertinoLocalizations.delegate,
-                    ],
-                    supportedLocales: [
-                      Locale('th'),
-                      Locale('en'),
-                    ],
-                    locale: localeStore.locale,
-                    navigatorKey: _navigatorKey,
-                    onGenerateRoute: (settings) =>
-                        MaterialPageRoute(
-                            builder: (context) => const SplashPage()),
-                  ),
-                ),
+      builder: (light, dark) => Observer(
+        builder: (context) => ReactiveFormConfig(
+          validationMessages: {
+            ValidationMessage.required: (error) => 'Should not be empty',
+            ValidationMessage.email: (error) => 'Invalid email',
+          },
+          child: MaterialApp(
+            title: 'My App',
+            theme: light,
+            darkTheme: dark,
+            localizationsDelegates: [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: [
+              Locale('th'),
+              Locale('en'),
+            ],
+            locale: localeStore.locale,
+            navigatorKey: _navigatorKey,
+            onGenerateRoute: (settings) =>
+                MaterialPageRoute(builder: (context) => const SplashPage()),
           ),
+        ),
+      ),
     );
   }
 }
--- lib/app/authentication/splash_page.dart
+++ lib/app/authentication/splash_page.dart
@@ -1,4 +1,3 @@
-import 'package:emer_app/app/authentication/logo_widget.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatefulWidget {
@@ -12,50 +11,7 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset('assets/images/shape.png'),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                    width: 180,
-                    child: Image.asset(
-                        'assets/images/image_32-removebg-preview.png')),
-                SizedBox(height: 20),
-                LogoWidget()
-              ],
-            ),
-          ),
-          Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('POWERED BY',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 12)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 15),
-                    child: SizedBox(
-                        height: 40,
-                        child:
-                            Image.asset('assets/images/DOTS BLACK LOGO 1.png')),
-                  ),
-                ],
-              ))
-        ],
-      ),
+      body: Center(child: CircularProgressIndicator()),
     );
   }
 }
